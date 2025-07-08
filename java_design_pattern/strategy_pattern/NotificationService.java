6a333b255cbc125f0bc7f49a2c5b93b10e2b6700
Vdtry-06 <tunhoipro0306@gmail.com>
2025-07-08 23:39:34 +0700

    add java strategy-pattern

---
+++ java_design_pattern/strategy_pattern/NotificationService.java
@@ -0,0 +1,17 @@
+package java_design_pattern.strategy_pattern;
+
+public class NotificationService {
+    private Notifier notifier;
+
+    public NotificationService(Notifier notifier) {
+        this.notifier = notifier;
+    }
+
+    public void SendNotification(String message) {
+        notifier.send(message);
+    }
+
+    public void setNotifier(Notifier notifier) {
+        this.notifier = notifier;
+    }
+}
---
+++ java_design_pattern/strategy_pattern/Notifier.java
@@ -0,0 +1,5 @@
+package java_design_pattern.strategy_pattern;
+
+public interface Notifier {
+    void send(String message);
+} 
